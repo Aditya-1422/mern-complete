56fb988741d1fcb3c0c2f0b1e7e4ccb3199897e3
Aditya Ozalwar <adityaozalwarcnp@gmail.com>
2024-12-03 14:14:42 +0530

    Made complete sign-in functionality and redux-toolkit setup

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,6 +1,7 @@
 import User from "../model/User.js";
 import bcrypt from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -34,3 +35,57 @@ export const signup = async (req, res, next) => {
     next(errorHandler(error));
   }
 };
+
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Email and password are required",
+    });
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User does not exist"));
+    }
+
+    const validPassword = await bcrypt.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(403, "Wrong credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.cookie('access_token', token, {httpOnly: true})
+      .status(200)
+      .json({
+        success: true,
+        message: "Login successful",
+        user: {
+          id: validUser._id,
+          username: validUser.username,
+          email: validUser.email,
+        },
+      });
+  } catch (error) {
+    next(errorHandler(500, "Internal Server Error"));
+  }
+};
+
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token',
+      {httpOnly : true}
+    ).status(200)
+    .json({
+      success : true,
+      message : "Signout successfully!!!"
+    })
+  } catch (error) {
+    next(error);
+  }
+}
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInFailure, signinStart, signInSuccess } from '../redux/user/userSlice';
 
 const SignIn = () => {
+  const [formData, setFormdata] = useState({});
+  const {loading, error} = useSelector((state) => state.user)
+  const [success, setSuccess] = useState(null);
+  const navigate = useNavigate();
+  const dispatch = useDispatch()
+
+  const handleChange = (e) => {
+    setFormdata({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(signinStart())
+    setSuccess(null); 
+    try {
+      const response = await fetch('/api/v1/signin', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if (!data.success) {
+        dispatch(signInFailure(data.message))
+        return;
+      }
+      setSuccess(data.message);
+      dispatch(signInSuccess(data))
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error.message))
+      setError('Failed to connect to the server.');
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="font-semibold text-center my-7 text-3xl">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          onChange={handleChange}
+          type="email"
+          placeholder="email"
+          id="email"
+          className="border p-3 rounded-lg"
+          required
+        />
+        <input
+          onChange={handleChange}
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg"
+          required
+        />
+        <button
+          disabled={loading}
+          className="uppercase bg-slate-800 text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-80"
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+      <div className="mt-5 gap-3">
+        <span>Dont have an account ? </span>
+        <Link to="/sign-up" className="text-blue-800 hover:underline">
+          Sign-up
+        </Link>
+      </div>
+      {error && <p className="text-red-700 mt-4">{error}</p>}
+      {success && <p className="text-green-700 mt-4">{success}</p>}
+    </div>
+  );
+};
 
 export default SignIn
